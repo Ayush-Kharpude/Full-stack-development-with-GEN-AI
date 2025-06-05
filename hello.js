00973c1d6d71c37626b3c2d787fd3410051766f2
Ayush-Kharpude <aayushkharpude@gmail.com>
2025-06-05 10:32:49 +0530

    basic functions

--- hello.js
+++ hello.js
@@ -1,9 +1,26 @@
-let a=11
-if(a%2==0){
-    console.log("The Number is even")
-}else{
-    console.log("The Number is odd")
+// let a=11
+// if(a%2==0){
+//     console.log("The Number is even")
+// }else{
+//     console.log("The Number is odd")
+// }
+// const prompt = require('prompt-sync')();
+// const name = prompt("What is your name? ");
+// console.log("Hello, " + name + "!");
+
+// function square(x) {
+//     let ans = 0;
+//     ans = x*x ;
+//     return ans;
+
+// }
+// console.log("The square of 5 is: " + square(5));
+
+const square= function(x=7){
+    let ans =0
+        ans = x*x ;
+    return ans;
+
 }
-const prompt = require('prompt-sync')();
-const name = prompt("What is your name? ");
-console.log("Hello, " + name + "!");
+console.log("The square of 5 is: " + square());
+
